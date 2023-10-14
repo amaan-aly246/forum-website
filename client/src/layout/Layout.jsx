9fd5419e498e4093c672ef30741eb3f5cd4a47f6
amaan <amaan.ali.ug22@nsut.ac.in>
2023-10-14 19:17:34 +0530

    commit

--- client/src/layout/Layout.jsx
+++ client/src/layout/Layout.jsx
@@ -3,7 +3,6 @@ import Header from "../components/Header/Header"
 import { Outlet } from "react-router-dom"
 export default function Layout() {
   return (
-    // this is the layout for the entire app
     <main>
       <Header />
       <Outlet />
