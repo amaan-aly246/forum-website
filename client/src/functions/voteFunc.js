570e1055f3c414f1deced9f0545a2743fdcc5b76
amaan <amaan.ali.ug22@nsut.ac.in>
2023-10-15 15:58:48 +0530

    improved voting

--- client/src/functions/voteFunc.js
+++ client/src/functions/voteFunc.js
@@ -2,26 +2,42 @@
 
 export function voteFunc(event, voteCount, setVoteCount, postID) {
 
-  const postElement = event.target.closest(`#post-${postID}`);
+  const postElement = event.target.closest(`.post-container`);
   const up = postElement.querySelector("#up");
   const down = postElement.querySelector("#down");
 
   if (event.target.id === "up") {
-    if (up.classList.contains("filter-gray")) {
-      setVoteCount(voteCount + 1);
-      up.classList.remove("filter-gray");
-    } else {
-      setVoteCount(voteCount - 1);
-      up.classList.add("filter-gray");
-    }
-  } else if (event.target.id === "down") {
-    if (down.classList.contains("filter-gray")) {
-      setVoteCount(voteCount - 1);
-      down.classList.remove("filter-gray");
-    } else {
-      setVoteCount(voteCount + 1);
+    if (!down.classList.contains("filter-gray")) {
+      setVoteCount(voteCount + 2);
       down.classList.add("filter-gray");
+      up.classList.remove("filter-gray");
     }
+    else {
+      if (up.classList.contains("filter-gray")) {
+        setVoteCount(voteCount + 1);
+        up.classList.remove("filter-gray");
+      } else {
+        setVoteCount(voteCount - 1);
+        up.classList.add("filter-gray");
+      }
+    }
+
+  } else if (event.target.id === "down") {
+    if (!up.classList.contains("filter-gray")) {
+      setVoteCount(voteCount - 2);
+      up.classList.add("filter-gray");
+      down.classList.remove("filter-gray");
+    }
+    else {
+      if (down.classList.contains("filter-gray")) {
+        setVoteCount(voteCount - 1);
+        down.classList.remove("filter-gray");
+      } else {
+        setVoteCount(voteCount + 1);
+        down.classList.add("filter-gray");
+      }
+    }
+
   }
 }
 
