5c05fb381e21be9a6e37e047a17c3183b1f2cb6d
amaan <amaan.ali.ug22@nsut.ac.in>
2023-10-15 23:47:37 +0530

    commit

--- client/src/components/Context/Context.jsx
+++ client/src/components/Context/Context.jsx
@@ -8,7 +8,7 @@ const DataProvider = ({ children }) => {
   const [specificPostData, setSpecificPostData] = useState({});
 
   const [voteCount, setVoteCount] = useState(0);
-  const url = 'https://forum-website-client.vercel.app'
+  const url = 'https://forum-website-api.onrender.com'
   
 
   return (
